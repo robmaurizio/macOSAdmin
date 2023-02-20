13df596748333579639e48caf88b8e4166018886
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2023-02-20 15:35:37 -0600

    Updated results

--- EA_fileExists.bash
+++ EA_fileExists.bash
@@ -3,8 +3,8 @@
 # Does file at /path/to/file exist?
 if [ -f /path/to/file ]
 then
-echo "<result>Yes</result>"
+echo "<result>Present</result>"
 else
-echo "<result>No</result>"
+echo "<result>Not Found</result>"
 fi
 exit 0
